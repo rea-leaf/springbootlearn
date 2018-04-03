b65b1e5137e2934a0add4351b290830b5d3e2831
温柔一刀 <1518300818@qq.com>
2018-04-03 22:41:26 +0800

    log

---
+++ springboot-config/src/test/java/com/chf/springbootconfig/test.java
@@ -0,0 +1,16 @@
+package com.chf.springbootconfig;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author 温柔一刀
+ * @create 2018-03-29 20:18
+ **/
+public class test {
+
+    public static void main(String[] args) {
+        String str="78275759";
+        System.out.println(str.substring(6,7));
+    }
+}
+
